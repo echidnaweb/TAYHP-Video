6731c86f9cd0fed9ea40d2d4a6b841ddbb0c9615
Richard Goulstone <richard@roxwood.com>
2012-06-28 21:14:02 +0100

    twitter calls and caching 75% there. Code half-baked.

--- processtimeline.js.php
+++ processtimeline.js.php
@@ -1,30 +1,33 @@
 <?php
 // Define Constants
-define("URL", "http://search.twitter.com/search.json?rpp=1");
+define("TWITTER_MAX_RESULTS",100);
+define("TWITTER_URL", "http://search.twitter.com/search.json?rpp=".TWITTER_MAX_RESULTS);
+define("TWITTER_MAX_CALLS", 100);
 define("CONFIG_PATH", getcwd()."/conf");
-define("FOO_BAR", "something more");
+define("CACHE_FILE_PATH", getcwd()."/cache/extdata.json");
 
-// Check that video ID has been passed in GET string
+// Define Globals
+$aTL= array(); //timeline data 
+$aExtDataCache = new ExtDataCache(CACHE_FILE_PATH,3600); //external data cache
+$APICalls = array();
+
+// Attempt to load timeline data
 if (!isset($_GET['video']))
+  $tljson = loadtljson($_GET['video']);
+else
 {
   echo "Video ID must be supplied in URL"; 
   exit();
 }
 
-// Attempt to load timeline data
-if(!$tljson = file_get_contents(CONFIG_PATH."/".$_GET['video'].".json"))
-{
-  echo "Error loading timeline data";
-  exit();
-}  
-
-// Decode json to a PHP array 
+// Decode json to PHP array
 $aTL=json_decode($tljson,true);
 
 // Trim timeline data array
 trimtldata($aTL);
 
-lookuptweets($aTL);
+// Look up external data
+lookupextdata($aTL);
 
 print_r($aTL);
 exit();
@@ -50,6 +53,16 @@ echo "<pre>";
 print_r(json_encode($results));
 echo "</pre>";
 
+function loadtljson($videoid)
+{
+  if(!$tljson = file_get_contents(CONFIG_PATH."/".$videoid.".json")
+  {
+    echo "Error loading timeline data";
+    exit();
+  }
+  return $tljson; 
+}
+
 function trimtldata(&$aTL)
 {
   $aTL = $aTL['media'][0];
@@ -65,8 +78,29 @@ function trimtldata(&$aTL)
   unset($aTL['tracks']);
 }
 
-function lookuptweets(&$aTL)
+/* look up event-specific external data via web services etc */
+function lookupextdata(&$aTL)
 {
+  foreach ($aTL['events'] as $event)
+  {
+    switch($event['type'])
+    {
+      case "twitter":
+        $event['popcornOptions']['text'] = lookuptweet($event['popcornOptions']['src']); 
+        break;
+      case "flickr":
+        break;
+    } 
+  }
+}
 
+function twtAPISearch($query)
+{
+  return "some text":
+}
+
+function getcachevalue($type,$key)
+{
+  if (isset
 }
 ?>
