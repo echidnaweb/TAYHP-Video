42b16c158344174bd840d88a5347986b9b80a37b
Richard Goulstone <richard@roxwood.com>
2012-07-15 22:02:32 +0100

    Wordriver now configurable for left > right , right > left , top > bottom , or bottom > top

--- class/WordRiverEvent.class.php
+++ class/WordRiverEvent.class.php
@@ -10,17 +10,31 @@ class WordRiverEvent
 {
   private $conf;
   private $js = "";
-  private $id,$start,$end,$target,$text,$template,$class,$style;
+  private $id,$start,$end,$target,$text,$template,$class,$style,$origin;
 
   function __construct($conf)
   {
     $this->conf = $conf; 
+//    print_r($conf);
     if ($this->preprocess()) $this->process();
   }
 
   private function process()
   {
+    switch ($this->origin)
+    {
+       case "left":
+       case "right":
+         $ststmt = "var start_point = '-'+$('#$this->target').width()+'px';";
+         $dtstmt = "var distance = $('#$this->target').width()+$(window).width()+'px';";
+       break;
 
+       case "top":
+       case "bottom":
+         $ststmt = "var start_point = '-'+$('#$this->target').height()+'px';";
+         $dtstmt = "var distance = $('#$this->target').height()+$(window).height()+'px';";
+       break;
+    }
     $this->js .= <<<EOF
     // Create a popcorn event
     popcorn.code({
@@ -29,22 +43,21 @@ class WordRiverEvent
        onStart: function( options ) {
 
          var text = '<span id=\'$this->id\' style=\'$this->style\' class=\'$this->class\'>$this->text</span>';
-
+         
          // If no template has been loaded yet or it has changed load template
          if (typeof window.template == 'undefined' || window.template != "$this->template")
          {
            window.template = "$this->template";
            $('#contentlayer').load('tpl/'+window.template+'.html', function()
            {
-             // get the left and width values of the target tag
-             var leftpx = parseInt($('#$this->target').css('left'));
-             var widthpx = parseInt($('#$this->target').css('width'));
-
              // set the class of the body tag to the template name
              $('body').attr('class',window.template);
 
-             // move the target tag offscreen to the left 
-             $('#$this->target').css('left','-'+widthpx+"px");
+             $ststmt
+             $dtstmt
+
+             // move the target tag offscreen 
+             $('#$this->target').css('$this->origin',start_point);
 
              // write the text to the target tag
              $('#$this->target').html(text);
@@ -53,32 +66,36 @@ class WordRiverEvent
              $('#$this->target #$this->id').show();
              
              //animate the target tag 
-             $('#$this->target').animate({"left": "+="+widthpx*2+"px"}, 19200);
+             $('#$this->target').animate({"$this->origin": "+="+distance}, 19200,'linear',function() { 
+               $('#$this->target').css('$this->origin',start_point); $('#$this->id').remove();
+             });
            });
          }
          else
          {
-           // get the left and width values of the target tag
-           var leftpx = parseInt($('#$this->target').css('left'));
-           var widthpx = parseInt($('#$this->target').css('width'));
+           $ststmt
+           $dtstmt
 
-           // move the target tag offscreen to the left 
-           $('#$this->target').css('left','-'+widthpx+"px");
+           // move the target tag offscreen
+           $('#$this->target').css('$this->origin',start_point);
 
            // write the text to the target tag
            $('#$this->target').html(text);
-           
+
            // un-hide the dynamic content
            $('#$this->target #$this->id').show();
 
            //animate the target tag
-           $('#$this->target').animate({"left": "+="+widthpx*2+"px"}, 19200);
+           $('#$this->target').animate({"$this->origin": "+="+distance}, 19200,'linear',function() {
+             $('#$this->target').css('$this->origin',start_point); $('#$this->id').remove()
+           ;});
          }
 
        },
        onEnd: function( options ) {
-        $('#$this->target').css('left','-'+widthpx+"px");
-        $('#$this->target #$this->id').remove();
+        //$ststmt
+        //$('#$this->target').css('$this->origin',start_point);
+        //$('#$this->id').remove();
        }
      });\n
 
@@ -97,10 +114,12 @@ EOF;
     $this->start = $this->conf['popcornOptions']['start'];
     $this->end = $this->conf['popcornOptions']['end'];
     $this->target = $this->conf['popcornOptions']['target'];
-    $this->text = addslashes(preg_replace("/[^a-zA-Z0-9 !@#\$%\^\*\?\.;&:\-\+=\/]/","",nl2br($this->conf['popcornOptions']['text'])));
+    //$this->text = addslashes(preg_replace("/[^a-zA-Z0-9 !@#\$%\^\*\?\.;&:\-\+=\/]/","",nl2br($this->conf['popcornOptions']['text'])));
+    $this->text = $this->conf['popcornOptions']['text'];
     $this->template = $this->conf['template'];
     $this->class = isset($this->conf['class'])?$this->conf['class']:"";
     $this->style = isset($this->conf['style'])?$this->conf['style']:"";
+    $this->origin = isset($this->conf['popcornOptions']['origin'])?$this->conf['popcornOptions']['origin']:"right";
     return true;
   }
 
