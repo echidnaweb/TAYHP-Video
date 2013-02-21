b5d3ac8bf7490f02bdef563ce7202a48a31363fc
Richard Goulstone <richard@roxwood.com>
2013-02-21 16:55:27 +0000

    Bugfixes Added ChangeClassPauseEvent finished Argentina config

---
+++ class/ChangeClassPauseEvent.class.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * ChangeClassPauseEvent class
+ * Changes the value of the class attribute of the body tag during a pause 
+ * Generates javascript code from config array 
+ *
+ **/
+
+class ChangeClassPauseEvent
+{
+  private $conf;
+  private $js = "";
+
+  function __construct($conf)
+  {
+    $this->conf = $conf; 
+    if ($this->preprocess()) $this->process();
+  }
+
+  private function process()
+  {
+     $delay = $this->delay*1000;
+
+     $classstatement = $this->class?"$('body').attr('class','$this->class');":"";
+   
+     $this->js .= <<<EOF
+
+         setTimeout(function() {
+           $classstatement
+         }, $delay);
+EOF;
+
+  }
+
+  private function preprocess()
+  {
+    $this->class = isset($this->conf['class'])?$this->conf['class']:false;
+    $this->delay = isset($this->conf['delay'])?$this->conf['delay']:0;
+
+    return true;
+  }
+
+  public function getJS()
+  {
+    return $this->js;
+  }
+
+}
+
+
+?>
