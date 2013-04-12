0712d1839704dfaf4beae49efbe2da59a84b45aa
Richard Goulstone <richard@roxwood.com>
2013-04-12 17:52:16 +0100

    bugfixes

--- class/HTMLEvent.class.php
+++ class/HTMLEvent.class.php
@@ -20,6 +20,19 @@ class HTMLEvent
 
   private function process()
   {
+    //concatenate html lines
+    if (is_array($this->html))
+    {
+      $html = "";
+      foreach ($this->html as $line)
+      {
+        $html .= $line;
+      }
+      $this->html = $html;
+    }
+
+    $this->html = addslashes($this->html);
+
     $id = uniqid("html_"); 
     $classstatement = $this->class?"$('body').attr('class','$this->class');":"";
     $this->js .= <<<EOF
@@ -28,10 +41,10 @@ class HTMLEvent
        start: $this->start,
        end: $this->end,
        onStart: function( options ) {
-         var html = '<div id=\'$id\'>addslashes($this->html)</div>';
+         var html = '<div id=\'$id\'>$this->html</div>';
          $classstatement
-         $('#$this->target').html(text);
-         $('#$this->target div#$id').fadeTo(1000, 0.999);
+         $('#$this->target').html(html);
+         $('#$this->target div#$id').fadeIn('slow');
        },
        onEnd: function( options ) {
         $('div#$id').fadeOut('slow', function() { $('div#$id').remove(); });
