14dd9c820f0b66f0f7673ecae3dfaa9acaeb21c2
Richard Goulstone <richard@roxwood.com>
2013-04-29 16:58:15 +0100

    Reordered thumbnails

--- thumbs.php
+++ thumbs.php
@@ -2,15 +2,17 @@
 
 // shows the list of videos and paths to their thumbnails
 $thumbs  = array(
-uganda => "http://b.vimeocdn.com/ts/420/738/420738487_100.jpg",      // UGANDA
-peru   => "http://b.vimeocdn.com/ts/420/717/420717525_100.jpg",			 // PERU
-wales  => "http://b.vimeocdn.com/ts/420/752/420752810_100.jpg",      // WALES
-maharashtra => "http://b.vimeocdn.com/ts/424/366/424366603_100.jpg", // MAHARASHTRA
-jharkhand => "http://b.vimeocdn.com/ts/420/264/420264171_100.jpg",   // JHARKHAND
+
 argentina => "http://b.vimeocdn.com/ts/420/778/420778755_100.jpg",   // ARGENTINA
+jharkhand => "http://b.vimeocdn.com/ts/420/264/420264171_100.jpg",   // JHARKHAND
+maharashtra => "http://b.vimeocdn.com/ts/424/366/424366603_100.jpg", // MAHARASHTRA
 mongolia => "http://b.vimeocdn.com/ts/420/945/420945390_100.jpg",    // MONGOLIA 
-tasmania => "http://b.vimeocdn.com/ts/424/507/424507443_100.jpg"     // TASMANIA
-);
+peru   => "http://b.vimeocdn.com/ts/420/717/420717525_100.jpg",			 // PERU
+tasmania => "http://b.vimeocdn.com/ts/424/507/424507443_100.jpg",    // TASMANIA
+uganda => "http://b.vimeocdn.com/ts/420/738/420738487_100.jpg",      // UGANDA
+wales  => "http://b.vimeocdn.com/ts/420/752/420752810_100.jpg");      // WALES
+
+
 $num_thumbs = count($thumbs);
 
 ?>
